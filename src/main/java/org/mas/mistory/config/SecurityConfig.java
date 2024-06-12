f42206b522df63d3678da734b0b85e7a5e4b814f
phinsso <naso01247@gmail.com>
2024-06-12 23:42:10 +0900

    #26 feat: cors 설정

--- src/main/java/org/mas/mistory/config/SecurityConfig.java
+++ src/main/java/org/mas/mistory/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -37,5 +42,22 @@ public class SecurityConfig {
                 .csrf((auth) -> auth.disable());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // 자격 증명을 허용
+        config.setAllowCredentials(true);
+        // 특정 출처 허용
+        config.addAllowedOrigin("http://127.0.0.1:5501");
+        // 모든 헤더 허용
+        config.addAllowedHeader("*");
+        // 허용할 HTTP 메서드 설정
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        // 모든 경로에 대해 위에서 설정한 cors 설정을 적용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
