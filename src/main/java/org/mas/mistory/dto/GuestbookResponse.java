c4e713129136e53063522cff8f62185b81404ea1
phinsso <naso01247@gmail.com>
2024-06-14 13:44:22 +0900

    #33 방명록 조회 포맷 형식 수정 (hh -> HH)

--- src/main/java/org/mas/mistory/dto/GuestbookResponse.java
+++ src/main/java/org/mas/mistory/dto/GuestbookResponse.java
@@ -22,7 +22,7 @@ public class GuestbookResponse {
     }
 
     private String formatDateTime(LocalDateTime dateTime) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd . hh:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd . HH:mm");
         return dateTime.format(formatter);
     }
 }
