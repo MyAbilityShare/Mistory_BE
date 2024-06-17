f8b2855d76542d09eacb3c12d8da5d341f2c39cd
HyunJin <myfamily11141@gmail.com>
2024-06-17 16:25:21 +0900

    #12 feat : Rank DTO 추가

--- src/main/java/org/mas/mistory/dto/RankResponse.java
+++ src/main/java/org/mas/mistory/dto/RankResponse.java
@@ -1,6 +1,23 @@
 package org.mas.mistory.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
 public class RankResponse {
     private String nickname;
-    private int score;
+    private LocalTime time;
+    private String endTime;
+
+
+    public RankResponse(String nickname, LocalTime time, LocalDateTime endTime) {
+        this.nickname = nickname;
+        this.time = time;
+        this.endTime = endTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+
 }
