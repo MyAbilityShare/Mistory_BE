58f530fd0262d05916af97e1d889591ac8b6c606
phinsso <naso01247@gmail.com>
2024-06-17 19:00:02 +0900

    #40 fix: 로그인 성공 후 랭킹 페이지로 리다이렉션 되게 수정

---
+++ src/main/java/org/mas/mistory/config/LoginFailHandler.java
@@ -0,0 +1,22 @@
+package org.mas.mistory.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        // log.info("Authentication failed: " + exception.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed");
+    }
+}
